7349ff8e7fe0647881baec035330b483deee04c2
abderrahimelamarty <abdrahim.tawnat12345@gmail.com>
2023-07-27 12:36:15 +0100

    Environment setup

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/get-billboard";
+
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("9d517e3d-4665-4738-b6ed-9743930d4aa8");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+         
+        </div>
+      </div>
+    </Container>
+  )
+};
+
+export default HomePage;
---
+++ components/navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+import MainNav from "@/components/main-nav";
+import Container from "@/components/ui/container";
+
+import getCategory from "@/actions/get-category";
+import NavbarActions from "./navbar-actions";
+export const revalidate=0;
+
+
+const Navbar = async () => {
+  const categories = await getCategory();
+ console.log(categories)
+
+  return ( 
+    <div className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">STORE</p>
+          </Link>
+          <MainNav data={categories} />
+         <NavbarActions/>
+        </div>
+      </Container>
+    </div>
+  );
+};
+ 
+export default Navbar;
